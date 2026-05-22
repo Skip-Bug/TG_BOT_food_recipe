5547f56ffe36213283907daf8f354cb3a4d2a6ba
Leonid Stoyko <gribojor@gmail.com>
2026-05-22 21:26:43 +0500

    fix variable name

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -3,16 +3,16 @@ from config import ADMIN_IDS
 
 
 def main_menu(user_id):
-    kb = [
+    buttons = [
         [InlineKeyboardButton("🍽 Рецепт", callback_data="random_recipe")],
         [InlineKeyboardButton("⭐ Избранное", callback_data="favorites")],
         [InlineKeyboardButton("💰 Подписка", callback_data="subscribe")]
     ]
 
     if user_id in ADMIN_IDS:
-        kb.append([InlineKeyboardButton("👑 Админка", callback_data="admin_panel")])
+        buttons.append([InlineKeyboardButton("👑 Админка", callback_data="admin_panel")])
 
-    return InlineKeyboardMarkup(kb)
+    return InlineKeyboardMarkup(buttons)
 
 
 def admin_menu():
