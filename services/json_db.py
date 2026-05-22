5b20da6c2685ac2a335d880d157e87dbc69cdb43
Leonid Stoyko <gribojor@gmail.com>
2026-05-22 20:57:49 +0500

    fix variable names

--- services/json_db.py
+++ services/json_db.py
@@ -1,32 +1,35 @@
 import json
 
+
 def load_recipes():
     try:
-        with open("database/recipes.json", "r", encoding="utf-8") as f:
-            return json.load(f)
+        with open("database/recipes.json", "r", encoding="utf-8") as file:
+            return json.load(file)
     except:
         return []
 
+
 def save_recipes(data):
-    with open("database/recipes.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+    with open("database/recipes.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
 
 
 def load_users():
     try:
-        with open("database/users.json", "r", encoding="utf-8") as f:
-            return json.load(f)
+        with open("database/users.json", "r", encoding="utf-8") as file:
+            return json.load(file)
     except:
         return []
 
+
 def save_users(data):
-    with open("database/users.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+    with open("database/users.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
 
 
 def get_user(tg_id):
     users = load_users()
-    for u in users:
-        if u["tg_id"] == str(tg_id):
-            return u
+    for user in users:
+        if user["tg_id"] == str(tg_id):
+            return user
     return None
